5b4c182ac66215248f440798f60e969a3a67ea70
Leonardo Biondo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-14 09:43:59 -0300

    Desenvolvida base da tela de listagem de headsets

--- app/src/main/java/io/lb/meubeats/ui/login/LoginActivity.kt
+++ app/src/main/java/io/lb/meubeats/ui/login/LoginActivity.kt
@@ -1,10 +1,12 @@
 package io.lb.meubeats.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import io.lb.meubeats.R
 import io.lb.meubeats.databinding.ActivityLoginBinding
+import io.lb.meubeats.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -15,6 +17,13 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupOnLoginClick()
+    }
 
+    private fun setupOnLoginClick() {
+        binding.included.btLogin.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
