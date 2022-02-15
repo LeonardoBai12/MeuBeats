9c1ae677411885eb073135fcac48ba4766ba87ef
Leonardo Biondo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-15 09:08:31 -0300

    Adicionada estrutura base de integração com Firebase

--- app/src/main/java/io/lb/meubeats/ui/headset/HeadsetViewModel.kt
+++ app/src/main/java/io/lb/meubeats/ui/headset/HeadsetViewModel.kt
@@ -2,11 +2,16 @@ package io.lb.meubeats.ui.headset
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.ValueEventListener
 import io.lb.meubeats.model.headset.Headset
+import io.lb.meubeats.network.headset.HeadsetRepository
 import io.lb.meubeats.utils.ResourceCreator
 import javax.inject.Inject
 
-class HeadsetViewModel @Inject constructor(): ViewModel() {
+class HeadsetViewModel @Inject constructor(
+    private val repository: HeadsetRepository
+): ViewModel() {
     var selectedHeadset = MutableLiveData<Headset?>()
     var headsets = MutableLiveData<ArrayList<Headset>>()
     var selectedPosition: Int? = null
@@ -14,5 +19,17 @@ class HeadsetViewModel @Inject constructor(): ViewModel() {
     fun loadHeadsets(): MutableLiveData<ArrayList<Headset>> {
         headsets.value = ResourceCreator.exampleHeadsets()
         return headsets
+    }
+
+    fun insertHeadset(
+        id: Int,
+        headset: Headset,
+        onCompleted: (Boolean, Exception?) -> Unit
+    ): Task<Void> {
+        return repository.insertHeadset(id, headset, onCompleted)
+    }
+
+    fun loadHeadsetsListener(onDataChanged: (ArrayList<Headset>) -> Unit): ValueEventListener {
+        return repository.loadHeadsetsListener(onDataChanged)
     }
 }
